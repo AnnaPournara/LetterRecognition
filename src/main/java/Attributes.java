d78d46af6669c4dbdbaed289677dc12e329b7b55
AnNable00 <101518922+AnNable00@users.noreply.github.com>
2022-07-03 14:17:02 +0300

    Update Attributes.java

--- src/main/java/Attributes.java
+++ src/main/java/Attributes.java
@@ -1,12 +1,11 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
-import java.lang.Math;
 
 public class Attributes {
 	public BufferedImage loadImage() {
 		try {
-			BufferedImage bufferedImage = ImageIO.read(new File("/Users/sissy/Desktop/Untitled.png"));
+			BufferedImage bufferedImage = ImageIO.read(new File("alpha.png"));
 			return bufferedImage;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -91,86 +90,6 @@ public class Attributes {
 	}
 	
 	
-	public String getAttr1(int [][] pixls) {
-		int attr1 = 0; //x-box
-		return Integer.toString(attr1);
-	}
-	
-	public String getAttr2(int [][] pixls) {
-		int attr2 = 0; //y-box
-		return Integer.toString(attr2);
-	}
-
-	public String getAttr3(int [][] pixls) {
-		int attr3 = 0; //width
-		return Integer.toString(attr3);
-	}
-
-	public String getAttr4(int [][] pixls) {
-		int attr4 = 0; //width
-		return Integer.toString(attr4);
-	}
-
-	public String getAttr5(int [][] pixls) {
-		int attr5 = 0; //width
-		return Integer.toString(attr5);
-	}
-
-	public String getAttr6(int [][] pixls) {
-		int attr6 = 0; //width
-		return Integer.toString(attr6);
-	}
-
-	public String getAttr7(int [][] pixls) {
-		int attr7 = 0; //width
-		return Integer.toString(attr7);
-	}
-
-	public String getAttr8(int [][] pixls) {
-		int attr8 = 0; //width
-		return Integer.toString(attr8);
-	}
-
-	public String getAttr9(int [][] pixls) {
-		int attr9 = 0; //width
-		return Integer.toString(attr9);
-	}
-
-	public String getAttr11(int [][] pixls) {
-		int attr11 = 0; //width
-		return Integer.toString(attr11);
-	}
-
-	public String getAttr10(int [][] pixls) {
-		int attr10 = 0; //width
-		return Integer.toString(attr10);
-	}
-
-	public String getAttr12(int [][] pixls) {
-		int attr12 = 0; //width
-		return Integer.toString(attr12);
-	}
-
-	public String getAttr13(int [][] pixls) {
-		int attr13 = 0; //width
-		return Integer.toString(attr13);
-	}
-
-	public String getAttr14(int [][] pixls) {
-		int attr14 = 0; //width
-		return Integer.toString(attr14);
-	}
-
-	public String getAttr15(int [][] pixls) {
-		int attr15 = 0; //width
-		return Integer.toString(attr15);
-	}
-
-	public String getAttr16(int [][] pixls) {
-		int attr16 = 0; //width
-		return Integer.toString(attr16);
-	}
-	
 	private int[] getCartesianCoordinates(int[] pos, int[][] table){
 		int[] coords = new int[2];
 		double[] center = new double[2];
@@ -191,5 +110,92 @@ public class Attributes {
 		
 		return coords;
 	}
+	
+	
+	public String getAttr1(int [] BB) {
+		int attr1 = BB[2]+(BB[3]-BB[2])/2; //x-box
+		return Integer.toString(attr1);
+	}
+	
+	public String getAttr2(int [] BB) {
+		int attr2 = BB[0]+(BB[1]-BB[0])/2; //y-box
+		return Integer.toString(attr2);
+	}
+	
+	public String getAttr3(int [] BB) {
+		int attr3 = BB[3]-BB[2]+1; //width of bb
+		return Integer.toString(attr3);
+	}
+	
+	public String getAttr4(int [] BB) {
+        int attr4 = BB[1]-BB[0]+1; //height of bb
+        return Integer.toString(attr4);
+    }
+
+    public String getAttr5(int [] BB, int [][] pixls) {
+        int attr5 = 0; //num of 1s
+        for(int i=BB[0]; i<=BB[1]; i++) {
+        	for(int j=BB[2]; j<=BB[3]; j++) {
+        		if(pixls[i][j]==1)
+        			attr5++;
+        	}
+        }
+        return Integer.toString(attr5);
+    }
+
+    public String getAttr6(int [][] pixls) {
+        int attr6 = 0; //width
+        return Integer.toString(attr6);
+    }
+
+    public String getAttr7(int [][] pixls) {
+        int attr7 = 0; //width
+        return Integer.toString(attr7);
+    }
+
+    public String getAttr8(int [][] pixls) {
+        int attr8 = 0; //width
+        return Integer.toString(attr8);
+    }
+
+    public String getAttr9(int [][] pixls) {
+        int attr9 = 0; //width
+        return Integer.toString(attr9);
+    }
+
+    public String getAttr11(int [][] pixls) {
+        int attr11 = 0; //width
+        return Integer.toString(attr11);
+    }
+
+    public String getAttr10(int [][] pixls) {
+        int attr10 = 0; //width
+        return Integer.toString(attr10);
+    }
+
+    public String getAttr12(int [][] pixls) {
+        int attr12 = 0; //width
+        return Integer.toString(attr12);
+    }
+
+    public String getAttr13(int [][] pixls) {
+        int attr13 = 0; //width
+        return Integer.toString(attr13);
+    }
+
+    public String getAttr14(int [][] pixls) {
+        int attr14 = 0; //width
+        return Integer.toString(attr14);
+    }
+
+    public String getAttr15(int [][] pixls) {
+        int attr15 = 0; //width
+        return Integer.toString(attr15);
+    }
+
+    public String getAttr16(int [][] pixls) {
+        int attr16 = 0; //width
+        return Integer.toString(attr16);
+    }
 	
 }
